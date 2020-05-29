bb28e50d7a262e43ca641057960e9c8d62149920
Zantos321 <eljoe1985@gmail.com>
2020-05-29 16:25:24 -0700

    set up search, favorites, and showall

--- src/App.js
+++ src/App.js
@@ -1,54 +1,118 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style/master.scss"; // applies global scss styles
 import { uiData } from "./data/ui";
+import FunctionUI from "./components/FunctionUI";
 
-export default class App extends Component {
+export default class App extends React.Component {
    constructor() {
       super();
       console.log(uiData);
+      this.state = {
+         isFavoritesChecked: false,
+         allFuncs: uiData,
+         displayedFuncs: uiData,
+      };
    }
+
+   toggleFavorites(e) {
+      this.setState({ isFavoritesChecked: !this.state.isFavoritesChecked });
+      const userInput = e.target.id;
+      console.log(userInput);
+      const allFuncs = [...this.state.allFuncs];
+      if (userInput === "viewMode-favorites") {
+         const filteredFuncs = allFuncs.filter((func) => {
+            return func.isFavorite;
+         });
+         console.log(filteredFuncs);
+         this.setState({ displayedFuncs: filteredFuncs });
+      } else {
+         this.setState({ displayedFuncs: allFuncs });
+      }
+   }
+
    render() {
-      // function renderInputs(num){} old school way
-      const renderInputs = (num) => {
-         // es6 way
-         const inputs = [];
-         for (let i = 0; i < num; i++) {
-            // render some jsx
-            inputs.push(
-               <input type="text" className="form-control inline-action" />
-            );
-         }
-         return inputs;
+      const getFunctionsNum = () => {
+         return uiData.length;
       };
       return (
          <div className="container">
             <div className="row">
-               {uiData.map((component) => {
-                  return (
-                     <div className="col-12 col-lg-8 offset-lg-2 mb-5">
-                        <p className="name">
-                           <b>{component.name}</b>&nbsp;-&nbsp;{component.desc}
-                        </p>
-                        <pre style={{ display: "none" }}>
-                           <code></code>
-                        </pre>
-                        <div className="actions float-right">
-                           {renderInputs(component.inputs)}
+               <div className="col-12 col-lg-8 offset-lg-2 mb5">
+                  <h1 className="d-flex justify-content-center">
+                     JavaScript Utility Library
+                  </h1>
+                  <p className="text-center lead mb-4">
+                     {getFunctionsNum()}&nbsp;functions documented
+                  </p>
+                  <div className="custom-control custom-radio custom-control-inline">
+                     <input
+                        type="radio"
+                        id="viewMode-all"
+                        name="viewMode"
+                        className="custom-control-input"
+                        checked={!this.state.isFavoritesChecked}
+                        onChange={(e) => {
+                           this.toggleFavorites(e);
+                        }}
+                     />
+                     <label
+                        className="custom-control-label"
+                        htmlFor="viewMode-all"
+                     >
+                        Show all
+                     </label>
+                  </div>
+                  <div className="custom-control custom-radio custom-control-inline">
+                     <input
+                        type="radio"
+                        id="viewMode-favorites"
+                        name="viewMode"
+                        className="custom-control-input"
+                        checked={this.state.isFavoritesChecked}
+                        onChange={(e) => {
+                           this.toggleFavorites(e);
+                        }}
+                     />
 
-                           <button className="btn btn-primary inline-action">
-                              Run
-                           </button>
-                        </div>
-                        <div className="clearfix mb-3"></div>
-                        <div
-                           className="alert alert-primary"
-                           style={{ display: "none" }}
-                        ></div>
-                        <div
-                           className="alert alert-danger"
-                           style={{ display: "none" }}
-                        ></div>
+                     <label
+                        className="custom-control-label"
+                        htmlFor="viewMode-favorites"
+                     >
+                        Favorites
+                     </label>
+                  </div>
+
+                  <div className="row mt-3">
+                     <div className="col-6">
+                        <input
+                           type="text"
+                           className="form-control"
+                           placeholder="Search all functions"
+                           aria-label="Search all functions"
+                           aria-describedby="button-addon2"
+                           id="search-input"
+                        />
                      </div>
+
+                     <div className="col-6">
+                        <select className="form-control">
+                           <option>Most recent</option>
+                           <option>Oldest</option>
+                           <option>A - Z</option>
+                           <option>Z - A</option>
+                        </select>
+                     </div>
+                  </div>
+               </div>
+               {this.state.displayedFuncs.map((functionUI) => {
+                  const { name, desc, inputs } = functionUI; // destructuring functionUI so we dont have to keep typeing it in for calls
+                  return (
+                     <FunctionUI
+                        key={name}
+                        name={name}
+                        desc={desc}
+                        inputs={inputs}
+                     />
                   );
                })}
             </div>
